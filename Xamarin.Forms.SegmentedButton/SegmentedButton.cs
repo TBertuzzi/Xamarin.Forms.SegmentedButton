e70a7e33f053fff8124011cc8d0cba4b113a956e
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2020-08-26 17:47:00 -0300

    :lipstick: Ajustado tela de reprovacao para idioma

--- Xamarin.Forms.SegmentedButton/SegmentedButton.cs
+++ Xamarin.Forms.SegmentedButton/SegmentedButton.cs
@@ -1,8 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
 namespace Xamarin.Forms.SegmentedButton
 {
     public class SegmentedButton
     {
-        public string Title { get; set; }
+        //public string Title { get; set; }
+
+        public static readonly BindableProperty DefaultColorProperty = BindableProperty.Create("Title", typeof(string), typeof(SegmentedButton), ");
+
+        public string Title
+        {
+            get { return (string)GetValue(DefaultColorProperty); }
+            set { SetValue(DefaultColorProperty, value); }
+        }
     }
 }
